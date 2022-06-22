d4313b7754efa1f354f71e16320a72a44c8094bd
kyopa <pomelleero@gmail.com>
2022-06-22 16:13:11 +0300

    jump and dest opcodes set

--- opcode.asm
+++ opcode.asm
@@ -1,6 +1,8 @@
 
 
 section .data
+
+
     null db "null", 0
     M db "M", 0
     D db "D", 0
@@ -9,6 +11,19 @@ section .data
     AM DB "AM", 0
     AD db "AD", 0
     ADM db "ADM", 0
+
+
+    jJGT db "JGT", 0
+    jJGE db "JGE", 0
+    jJEQ db "JEQ", 0
+    jJLT db "JLT", 0
+    jJNE db "JNE", 0
+    jJLE db "JLE", 0
+    jJMP db "JMP", 0
+
+    zero db "0", 0
+
+
 section .text
     
 %macro strcmp 2
@@ -18,9 +33,14 @@ section .text
     repe cmpsb
 %endmacro
 
+    ; OPCODE FOR COMP
+    compop:
+        print comp, 4
 
+
+
+    ; OPCODE FOR DESTINATION
     destop:
-
         strcmp dest, null
         jnz n1
         mov dword [op], "000"
@@ -61,6 +81,47 @@ section .text
         mov dword [op], "111"
 
         finish:
+        ret
+
+
+    ; OPCODE FOR JUMP FIELD
+    jumpop:
+        push rdi
+        mov byte [jump + 3], 0
+        strcmp jump, jJGE
+        jnz .n1
+        mov dword [op], "011"
+        .n1:
+        strcmp jump, null
+        jnz .n2
+        mov dword [op], "000"
+        .n2:
+        strcmp jump, jJGT
+        jnz .n3
+        mov dword [op], "001"
+        .n3:
+        strcmp jump, jJLT
+        jnz .n4
+        mov dword [op], "100"
+        .n4:
+        strcmp jump, jJNE
+        jnz .n5
+        mov dword [op], "101"
+        .n5:
+        strcmp jump, jJLE
+        jnz .n6
+        mov dword [op], "110"
+        .n6:
+        strcmp jump, jJMP
+        jnz .n7
+        mov dword [op], "111"
+        .n7:
+        pop rdi
+        ret
+
+
+        
+
 
 
         
